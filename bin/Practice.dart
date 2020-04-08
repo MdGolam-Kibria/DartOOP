eca0e9403ea20788fad34f5214a11ae96cb4ddb7
Kibria92 <kibria92@gmail.com>
2020-04-09 02:52:01 +0600

    learn all OOP concept for dart

---
+++ bin/Practice.dart
@@ -0,0 +1,16 @@
+void main(){
+
+}
+abstract class myClassTwo{
+  void m();
+  void n(){
+    print("This is abstract class method");
+  }
+}
+class b extends myClassTwo{
+  @override
+  void m() {
+
+  }
+  
+}
