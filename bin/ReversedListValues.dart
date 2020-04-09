b827f03f0aaf77ee484d3f50d8e1368a6a879ef4
Kibria92 <kibria92@gmail.com>
2020-04-09 22:31:26 +0600

    reversed a list values

---
+++ bin/ReversedListValues.dart
@@ -0,0 +1,10 @@
+void main(){
+  var list = new List();
+  list.add(15);
+  list.add(12);
+  list.add(200);
+  list.add(13);
+  list.add(14);
+  list.add(100);
+  print("The list values in reverse order: ${list.reversed}");//ei method dara list er man gula ke last to first dehkabe.
+}
